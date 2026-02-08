a2c93fc8df5e17ece57d00b921f7a74500edecaa
Pavan Kumar Thumburu <thumburupavankumar@gmail.com>
2026-02-09 00:51:21 +0530

    Add files via upload

---
+++ Full Stack AI/Data Types/06_dictionary_assignment/01_dictionary.py
@@ -0,0 +1,9 @@
+# Question 1: Create a dictionary of student names and their ages
+
+students = {
+    'Abhishek' : 23,
+    'Rupesh' : 28,
+    'Pavan' : 24
+}
+
+print(f"Dictionary of student names and their ages : {students}")

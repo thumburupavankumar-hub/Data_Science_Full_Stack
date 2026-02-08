6e500df1865b04f463261717034d54a60eb1af94
Pavan Kumar Thumburu <thumburupavankumar@gmail.com>
2026-02-08 07:48:42 +0530

    Add files via upload

---
+++ Full Stack AI/Data Types/02_float_assignment/02_float.py
@@ -0,0 +1,6 @@
+# Question 2: Convert 98.6 Fahrenheit to Celsius (F = C * 9/5 + 32)
+
+F = float(input("Enter Fahrenheit: "))
+C = (F-32) * 5/9
+
+print(f"Temp of celcius : {C:.2f} degrees celcius")
